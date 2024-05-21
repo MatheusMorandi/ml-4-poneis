41b107d33c9d5a286420625df76d31e50aa37080
MatheusMorandi <theu.morandi@gmail.com>
2024-05-21 11:04:01 -0300

    Primeira parte do segundo dia

---
+++ dia02/clones.py
@@ -0,0 +1,75 @@
+# %%
+
+import pandas as pd
+
+df = pd.read_parquet("../data/dados_clones.parquet")
+
+df
+# %%
+
+df.groupby(["Status "])[["Estatura(cm)", "Massa(em kilos)"]].mean()
+
+# %%
+
+df["Status_bool"] = df["Status "] == "Apto"
+
+df
+
+# %%
+
+df.groupby(["Distância Ombro a ombro"])["Status_bool"].mean()
+
+# %%
+
+df.groupby(["Tamanho do crânio"])["Status_bool"].mean()
+
+# %%
+
+df.groupby(["Tamanho dos pés"])["Status_bool"].mean()
+
+# %%
+
+df.groupby(["General Jedi encarregado"])["Status_bool"].mean()
+
+# %%
+
+features = [
+    "Massa(em kilos)",
+    "Estatura(cm)",
+    "Distância Ombro a ombro",
+    "Tamanho do crânio", 
+    "Tamanho dos pés"]
+
+cat_features = [
+    "Distância Ombro a ombro",
+    "Tamanho do crânio", 
+    "Tamanho dos pés"]
+
+x = df[features]
+
+from feature_engine import encoding
+
+onehot = encoding.OneHotEncoder(variables= cat_features)
+
+onehot.fit(x)
+
+x = onehot.transform(x)
+
+x
+
+# %%
+
+from sklearn import tree
+
+arvore = tree.DecisionTreeClassifier()
+
+arvore.fit(x, df["Status "])
+
+# %%
+
+tree.plot_tree(arvore,
+                class_names= arvore.classes_,
+                feature_names= x.columns,
+                filled= True)
+
+# %%
