41c30becbef7c77b40a354eac6e395e804c47ff4
MatheusMorandi <theu.morandi@gmail.com>
2024-05-27 10:29:53 -0300

    Iniciando a regressao linear

--- dia03/notas_cervejas.py
+++ dia03/notas_cervejas.py
@@ -28,3 +28,35 @@ plt.show()
 
 # %%
 
+from sklearn import linear_model
+
+rl = linear_model.LinearRegression()
+
+rl.fit(df[["cerveja"]], df["nota"])
+
+# %%
+
+x = df[["cerveja"]].drop_duplicates()
+
+y_estimado = rl.predict(x)
+
+
+plt.plot(df["cerveja"], df["nota"], "o")
+
+plt.plot(x, y_estimado, "-")
+
+plt.grid(True)
+
+plt.title("Relação entre Nota e quantidade de Cervejas")
+
+plt.ylim(0, 11)
+
+plt.xlim(0, 11)
+
+plt.ylabel("Nota")
+
+plt.xlabel("Cerveja")
+
+plt.show()
+
+# %%
