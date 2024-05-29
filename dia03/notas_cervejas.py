51451bdaa501204071800b2cea84f478f1150cf0
MatheusMorandi <theu.morandi@gmail.com>
2024-05-29 15:19:58 -0300

    Finalizando a Regressao e Arvore de decisao

--- dia03/notas_cervejas.py
+++ dia03/notas_cervejas.py
@@ -60,3 +60,38 @@ plt.xlabel("Cerveja")
 plt.show()
 
 # %%
+
+from sklearn import tree 
+
+arvore = tree.DecisionTreeRegressor(max_depth=2)
+
+arvore.fit(df[["cerveja"]], df["nota"])
+
+# %%
+
+y_estimado_arvore = arvore.predict(x)
+
+plt.plot(df["cerveja"], df["nota"], "o")
+
+plt.plot(x, y_estimado, "-")
+
+plt.plot(x, y_estimado_arvore, "-")
+
+plt.grid(True)
+
+plt.title("Relação entre Nota e quantidade de Cervejas")
+
+plt.ylim(0, 11)
+
+plt.xlim(0, 11)
+
+plt.ylabel("Nota")
+
+plt.xlabel("Cerveja")
+
+plt.legend(["Pontos", "Regressão Linear", "Árvore de Decisão"])
+
+plt.show()
+
+
+# %%
